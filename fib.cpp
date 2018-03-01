ee2cfb2c1d9cdbe499cdf4ddb70f1007ad49f8ca
krozet <keawa.rozet@yahoo.com>
2018-03-01 15:39:58 -0800

    KR01 - Program outline.

---
+++ fib.cpp
@@ -0,0 +1,55 @@
+/*
+Keawa Rozet
+CSc 600-01 - Programming Languages
+Homework #2
+Iteration vs Recursion 2
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits>
+
+typedef std::numeric_limits< double > dbl;
+
+int Frec(int n);
+int Fit(int n);
+void fibTimeMeasurement(int n);
+
+int main(int argc, char const** argv) {
+  fibTimeMeasurement(20);
+  
+  return 0;
+}
+
+int Frec(int n) {
+  return (n < 2) ? n : Frec(n-1) + Frec(n-2);
+}
+
+int Fit(int n) {
+  int i, zero = 0, first = 1, second = n;
+  for (i = 2; i <= n; i++) {
+    second = first + zero;
+    zero = first;
+    first = second;
+  }
+  return second;
+}
+
+void fibTimeMeasurement(int n) {
+  //first test binarySearchIteration
+  clock_t begin = clock();
+  int value = Fit(n);
+  clock_t end = clock();
+  double fit_elapsed_secs = double(end - begin)/CLOCKS_PER_SEC;
+  std::cout.precision(dbl::max_digits10);
+  std::cout << "Fib Iteration = " << value << " for n= " << n <<": " << std::fixed << fit_elapsed_secs << " seconds." << std::endl;
+
+  //second test binarySearchRecursion
+  clock_t begin2 = clock();
+  int value2 = Frec(n);
+  clock_t end2 = clock();
+  double frec_elapsed_secs = double(end2 - begin2)/CLOCKS_PER_SEC;
+  std::cout.precision(dbl::max_digits10);
+  std::cout << "Fib Recursion = " << value2 << " for n= " << n <<": " << std::fixed << frec_elapsed_secs << " seconds." << std::endl;
+  }
